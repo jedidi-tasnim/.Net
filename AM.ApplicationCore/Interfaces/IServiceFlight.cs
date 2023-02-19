977e086e6c13aed8abcf58149a3736a76b0d9a3b
jedidi-tasnim <tasnim.jedidi@esprit.tn>
2023-02-20 00:22:38 +0100

    first commit

---
+++ AM.ApplicationCore/Interfaces/IServiceFlight.cs
@@ -0,0 +1,24 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plane = System.Numerics.Plane;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public interface IServiceFlight
+    {
+        public List<DateTime> GetFlightDates(string destination);
+
+        void ShowFlightDetails(Plane plane);
+        int ProgrammedFlightNumber(DateTime startDate);
+        Double DurationAverage(string destination);
+        List<Flight> OrderedDurationFlights();
+        List<Traveller> SeniorTravellers(Flight flight);
+
+        void DestinationGroupedFlights();
+        public void GetFlights(string filterType, string filterValue);
+    }
+}
